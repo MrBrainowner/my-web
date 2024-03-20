28ac16c2292c2d84f25508c63b987b86c3a6797c
MrBrainowner <kenthendico011@gmail.com>
2024-03-21 00:34:58 +0800

    controllers and views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\indexController;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\productsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [indexController::class, 'index'] );
+Route::get('/about', [PagesController::class, 'about'] );
+Route::get('/contact', [PagesController::class, 'contact'] );
+Route::get('/products', [productsController::class, 'products'] );
+
