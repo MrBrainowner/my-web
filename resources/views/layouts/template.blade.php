7f5a2f6842c10cd78715f1d2cf4a071d58d6f6c3
MrBrainowner <kenthendico011@gmail.com>
2024-03-22 04:26:27 +0800

    content

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -14,6 +14,7 @@
     <title>Document</title>
 </head>
 <body>
+    {{-- nav bar --}}
     <nav class="nav">
       <div class="logo"><a href="/"><svg class="svg" width="30px" height="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 97.15 111.46"><defs><style>.cls-1{fill:#3f7e55;}.cls-2{fill:#abd486;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M67.39,34.73C56.31,36.39,45,37.14,31.69,48.14c-23.15,19.16-15.19,62-15,63.08-.68-1-29.28-43.17-10.1-69.56C26,15,48.66,13.75,48.26,0,48.26,0,61,15.11,67.39,34.73Z"/><path class="cls-2" d="M93.17,24.27C89.59,41.11,80.13,56.25,69,69a0,0,0,0,0,0,0c-22.46,25.82-51.62,41.83-52.3,42.2h0v0c-.21-1.12-8.17-43.92,15-63.08,13.28-11,24.62-11.75,35.7-13.41C75.88,33.45,84.22,31.64,93.17,24.27Z"/><path class="cls-1" d="M16.72,111.24h0c.68-.37,29.84-16.38,52.3-42.2a0,0,0,0,1,0,0c11.09-12.76,20.55-27.9,24.13-44.74C93.17,24.27,123.51,116.49,16.72,111.24Z"/></g></g></svg>    LeafyVerse</a></div>
       <div class="nav-list">
@@ -31,6 +32,9 @@
         </ul>
       </div>
     </nav>
-        @yield('content')
+      {{-- yield content --}}
+      @yield('content')
+      {{-- footer --}}
+    <div class="footer"></div>
 </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,29 @@
 @extends('layouts.template')
 @section('content')
-    <div class="hero-container">
-        
+    {{-- hero section --}}
+    <div class="hero-container"></div>
+    {{-- introductions --}}
+    <div class="intro container-fluid">
+        <h1>Lorem Ipsum</h1>
+        <div class="boxes container-fluid">
+            <div>
+                <h2>lorem</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta ducimus, magni tempora velit expedita, numquam, similique nemo ab eveniet at quam officia modi laborum itaque eius natus repellendus nisi! Ipsam fugit, incidunt, ad culpa inventore repudiandae non ducimus saepe pariatur excepturi accusamus, beatae a sed ex odio enim quisquam esse!</p>
+            </div>
+            <div>
+                <h2>lorem</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta ducimus, magni tempora velit expedita, numquam, similique nemo ab eveniet at quam officia modi laborum itaque eius natus repellendus nisi! Ipsam fugit, incidunt, ad culpa inventore repudiandae non ducimus saepe pariatur excepturi accusamus, beatae a sed ex odio enim quisquam esse!</p>
+            </div>
+            <div>
+                <h2>lorem</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta ducimus, magni tempora velit expedita, numquam, similique nemo ab eveniet at quam officia modi laborum itaque eius natus repellendus nisi! Ipsam fugit, incidunt, ad culpa inventore repudiandae non ducimus saepe pariatur excepturi accusamus, beatae a sed ex odio enim quisquam esse!</p>
+            </div>
+        </div>
     </div>
+    {{-- mission and vision --}}
+    <div class="m-v"></div>
+    {{-- call to action like sign up with email --}}
+    <div class="cta"></div>
+    {{-- community reviews --}}
+    <div class="com-rev"></div>
 @endsection
