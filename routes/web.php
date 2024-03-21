faf9dc25a09afccc7a69e457a2cf4ce0faec9eed
MrBrainowner <kenthendico011@gmail.com>
2024-03-21 12:27:19 +0800

    nav

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\accountController;
 use App\Http\Controllers\indexController;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\productsController;
@@ -9,4 +10,5 @@ Route::get('/', [indexController::class, 'index'] );
 Route::get('/about', [PagesController::class, 'about'] );
 Route::get('/contact', [PagesController::class, 'contact'] );
 Route::get('/products', [productsController::class, 'products'] );
+Route::get('/account', [accountController::class, 'account'] );
 
